cbf9c49de0eccea28b38ad0720116fa0ef7cd75c
pkaththr <piumali.kaththriarachchi@civica.co.uk>
2020-12-01 16:04:45 +0000

    feedback commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Powerapps-feedback-form" 
---
+++ Table.sql
@@ -0,0 +1,13 @@
+create table Employee(EmpId int primary key,EmpName varchar(max) not null,JobRole varchar(max) not null, ProjectName varchar(max) not null,AssessorName varchar(max) not null,
+PeriodCovered varchar(max) not null,TechnicalCapability varchar(max) not null,TechnicalCapabilityNotes varchar(max),Teamwork varchar(max) not null,TeamworkNotes varchar(max),
+Communication varchar(max) not null,CommunicatinoNotes varchar(max),PersonalDevNotes varchar(max),OverallPerformance varchar(max) not null,OverallPerformanceNotes varchar(max),
+Recommendations varchar(max)
+
+)
+
+
+
+
+
+
+
