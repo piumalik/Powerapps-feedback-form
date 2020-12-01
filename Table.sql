50bbd849c869252b2d5139d5c2e1f817717b6ad3
pkaththr <piumali.kaththriarachchi@civica.co.uk>
2020-12-01 16:21:14 +0000

    feedback

--- Table.sql
+++ Table.sql
@@ -2,7 +2,6 @@ create table Employee(EmpId int primary key,EmpName varchar(max) not null,JobRol
 PeriodCovered varchar(max) not null,TechnicalCapability varchar(max) not null,TechnicalCapabilityNotes varchar(max),Teamwork varchar(max) not null,TeamworkNotes varchar(max),
 Communication varchar(max) not null,CommunicatinoNotes varchar(max),PersonalDevNotes varchar(max),OverallPerformance varchar(max) not null,OverallPerformanceNotes varchar(max),
 Recommendations varchar(max)
-
 )
 
 
